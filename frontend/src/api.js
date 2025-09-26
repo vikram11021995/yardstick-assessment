f7bf8e6f38bfe77ef3d9a4f566ca460b4b8f1be0
Vikram Kumar <vikramiitr95@gmail.com>
2025-09-26 12:26:53 +0530

    fix:added .env.example file & url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const token = localStorage.getItem("token");
 
 export default axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: "http://localhost:4000",
   headers: { Authorization: `Bearer ${token}` },
 });
 
